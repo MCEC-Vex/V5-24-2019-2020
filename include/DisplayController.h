72975fa478e0e8f871c6375a2e6faecad75c0da0
TheTechdoodle <admin+github@darkender.com>
2020-01-19 15:02:58 -0800

    Document DisplayController and begin workspace for VSCode

--- include/DisplayController.h
+++ include/DisplayController.h
@@ -7,20 +7,51 @@
 class DisplayController
 {
 private:
+    // The controller to display lines on
     pros::Controller controller;
+    // Mutex to ensure thread safety with modifying lines
     pros::Mutex mutex;
+    // The most recently updated line. Starts at -2 because rumble is considered -1
     int lastUpdatedLine = -2;
+    // The 3 string lines and boolean flags to track modification
     std::string lines[3];
     bool modified[3];
+    // The rumble line. Emptiness indicates no modification
     std::string rumbleLine;
 
 public:
+    /**
+     * Construct a new Display Controller object
+     * 
+     * @param controller The controller to use
+     */
     DisplayController(pros::Controller controller);
 
+    /**
+     * Clears a line on the controller by writing 16 spaces
+     * 
+     * @param line The line number to clear (1-2)
+     */
     void clearLine(int line);
+
+    /**
+     * Set the Line text and add it to the update cycle
+     * 
+     * @param line The line number to set (1-2)
+     * @param data The string to write. Can be up to 16 characters
+     */
     void setLine(int line, std::string data);
+
+    /**
+     * Adds rumbling to the update cycle
+     * 
+     * @param pattern The rumble pattern of up to 8 characters where . = short, - = long, and " " = pause
+     */
     void rumble(std::string pattern);
 
+    /**
+     * Runs the next iteration of the update cycle. This should only be run every 50 milliseconds
+     */
     void sendNext();
 };
 
--- src/DisplayController.cpp
+++ src/DisplayController.cpp
@@ -2,18 +2,16 @@
 
 DisplayController::DisplayController(pros::Controller controller1) : controller(controller1)
 {
-    //pros::Task testingTask(this->runTask, (void*)"", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT);
+    
 }
 
 void DisplayController::sendNext()
 {
-    //double rand = random();
-    //controller.set_text(0, 0, std::to_string(rand).c_str());
-
     mutex.take(1000);
 
+    // Do a cycle that ends at the item most recently updated
+    // This gives all the lines (and rumble) a chance to be updated in a timely manner regardles of write frequency
     bool didUpdate = false;
-
     int current = lastUpdatedLine;
     for(int i = 0; i < 4; i++)
     {
@@ -23,9 +21,10 @@ void DisplayController::sendNext()
             current = -1;
         }
 
+        // Check if the rumble line is set
         if(current == -1 && !rumbleLine.empty())
         {
-            // Send rumble
+            // Send rumble (line -1)
             controller.rumble(rumbleLine.c_str());
             rumbleLine = "";
             lastUpdatedLine = -1;
@@ -35,6 +34,7 @@ void DisplayController::sendNext()
         {
             if(modified[current])
             {
+                // Send the new line and switch the modified flag off
                 controller.set_text(current, 0, lines[current].c_str());
                 modified[current] = false;
                 lastUpdatedLine = current;
@@ -42,36 +42,6 @@ void DisplayController::sendNext()
             }
         }
     }
-
-    /*if(!rumbleLine.empty() && lastUpdatedLine != -1)
-    {
-        controller.rumble(rumbleLine.c_str());
-        rumbleLine = "";
-        lastUpdatedLine = -1;
-        didUpdate = true;
-    }
-    else
-    {
-        // Go through the lines
-        for(int i = 0; i < 3; i++)
-        {
-            if(modified[i] && lastUpdatedLine != i)
-            {
-                controller.set_text(i, 0, lines[i].c_str());
-
-                modified[i] = false;
-                lastUpdatedLine = i;
-                didUpdate = true;
-                break;
-            }
-        }
-    }
-
-    if(!didUpdate)
-    {
-        lastUpdatedLine = -2;
-    }*/
-
     mutex.give();
 }
 
@@ -93,7 +63,6 @@ void DisplayController::setLine(int line, std::string data)
 
     //lines[line] = data.insert(data.length(), 16 - data.length(), ' ');
     lines[line] = data;
-
     modified[line] = true;
 
     mutex.give();
@@ -101,8 +70,8 @@ void DisplayController::setLine(int line, std::string data)
 
 void DisplayController::clearLine(int line)
 {
-    // 10 spaces for character limit
-    setLine(line, "          ");
+    // 16 spaces for character limit
+    setLine(line, "                ");
 }
 
 void DisplayController::rumble(std::string pattern)
